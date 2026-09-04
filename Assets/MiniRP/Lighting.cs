dc4e458636d734697b762997a45c26bbe55a99bd
qqshnep <qqshnep@gmail.com>
2026-09-05 04:04:24 +0800

    多光源

--- Assets/MiniRP/Lighting.cs
+++ Assets/MiniRP/Lighting.cs
@@ -3,14 +3,36 @@ using UnityEngine.Rendering;
 
 public class Lighting
 {
+    //主光源，平行光
     private static readonly int MainLightDirectionId = Shader.PropertyToID("_MainLightDirection");
 
     private static readonly int MainLightColorId = Shader.PropertyToID("_MainLightColor");
+
+
+    //次光源，暂时只考虑点光源
+    private const int MaxOtherLightCount = 4; //最多4个
+    private static readonly int OtherLightCountId = Shader.PropertyToID("_OtherLightCount");
+
+    private static readonly int OtherLightPositionsId = Shader.PropertyToID("_OtherLightPositions");
+
+    private static readonly int OtherLightColorsId = Shader.PropertyToID("_OtherLightColors");
+
+    private static readonly int OtherLightParamsId = Shader.PropertyToID("_OtherLightParams");
+
+    private readonly Vector4[] otherLightPositions = new Vector4[MaxOtherLightCount];
+
+    private readonly Vector4[] otherLightColors = new Vector4[MaxOtherLightCount];
+
+    private readonly Vector4[] otherLightParams = new Vector4[MaxOtherLightCount];
+
 
     public void Setup( ScriptableRenderContext context, CullingResults cullingResults)
     {
         Vector4 lightDirection = Vector4.zero;
         Color lightColor = Color.black;
+
+        int otherLightCount = 0;
+        bool findMainLight = false; //目前只使用第一盏方向光
 
         var visibleLights = cullingResults.visibleLights;
 
@@ -20,27 +42,37 @@ public class Lighting
 
             if (visibleLight.lightType == LightType.Directional)
             {
-                //Unity 的 Transform Matrix4x4 前三列分别对应 X、Y、Z 轴
-                //左手坐标系，Z+轴是前方
-                Vector4 forward = visibleLight.localToWorldMatrix.GetColumn(2);
+                if (!findMainLight)
+                {
+                    //Unity 的 Transform Matrix4x4 前三列分别对应 X、Y、Z 轴
+                    //左手坐标系，Z+轴是前方
+                    Vector4 forward = visibleLight.localToWorldMatrix.GetColumn(2);
 
-                // Shader 中使用的是：
-                // surface → light
-                //
-                // Directional Light 的 forward 是：
-                // light → scene
-                //
-                // 所以取反
-                lightDirection = new Vector4(
-                    -forward.x,
-                    -forward.y,
-                    -forward.z,
-                    0.0f
-                );
+                    // Shader 中使用的是：
+                    // surface → light
+                    //
+                    // Directional Light 的 forward 是：
+                    // light → scene
+                    //
+                    // 所以取反
+                    lightDirection = new Vector4(
+                        -forward.x,
+                        -forward.y,
+                        -forward.z,
+                        0.0f
+                    );
 
-                lightColor = visibleLight.finalColor;
+                    lightColor = visibleLight.finalColor;
 
-                break;
+                    findMainLight = true;
+                }
+            }
+            else if (visibleLight.lightType == LightType.Point &&
+                    otherLightCount < MaxOtherLightCount)
+            {
+                SetupPointLight(otherLightCount, visibleLight);
+
+                otherLightCount++;
             }
         }
 
@@ -49,12 +81,46 @@ public class Lighting
                 name = "Setup Lighting"
             };
 
+        //平行光
         cmd.SetGlobalVector( MainLightDirectionId, lightDirection);
-
         cmd.SetGlobalColor( MainLightColorId, lightColor );
+
+        //点光
+        cmd.SetGlobalInt(OtherLightCountId, otherLightCount);
+        cmd.SetGlobalVectorArray(OtherLightPositionsId, otherLightPositions);
+        cmd.SetGlobalVectorArray(OtherLightColorsId, otherLightColors);
+        cmd.SetGlobalVectorArray(OtherLightParamsId, otherLightParams);
 
         context.ExecuteCommandBuffer(cmd);
 
         cmd.Release();
+    }
+
+
+    private void SetupPointLight(int index, VisibleLight visibleLight)
+    {
+        //第四列，是world pos
+        Vector4 position = visibleLight.localToWorldMatrix.GetColumn(3);
+
+        //w=1,齐次坐标变换需要
+        otherLightPositions[index] = new Vector4(
+                position.x,
+                position.y,
+                position.z,
+                1.0f
+            );
+
+        otherLightColors[index] = visibleLight.finalColor;
+
+        float range = visibleLight.range;
+
+        float inverseRangeSquared = 1.0f / Mathf.Max(range * range, 0.00001f);
+
+        otherLightParams[index] = new Vector4(
+                inverseRangeSquared,
+                0,
+                0,
+                0
+            );
     }
 }
--- Assets/MiniRP/Shader/MiniLit.shader
+++ Assets/MiniRP/Shader/MiniLit.shader
@@ -30,8 +30,6 @@ Shader "MiniRP/MiniLit"
             struct Attributes
             {
                 float4 posOS : POSITION;
-
-                // 新增
                 float3 normalOS : NORMAL;
             };
 
@@ -39,8 +37,9 @@ Shader "MiniRP/MiniLit"
             {
                 float4 posCS : SV_POSITION;
 
-                // 新增,世界空间计算光照
-                float3 normalWS : TEXCOORD0;
+                // 世界空间计算光照
+                float3 posWS : TEXCOORD0;
+                float3 normalWS : TEXCOORD1;
             };
 
 
@@ -63,9 +62,40 @@ Shader "MiniRP/MiniLit"
             float4 _MainLightColor;
 
 
+            #define MAX_OTHER_LIGHT_COUNT 4
+            int _OtherLightCount;
+            float4 _OtherLightPositions[MAX_OTHER_LIGHT_COUNT];
+            float4 _OtherLightColors[MAX_OTHER_LIGHT_COUNT];
+            float4 _OtherLightParams[MAX_OTHER_LIGHT_COUNT];
+
+            float3 CalculatePointLight(int lightIndex, float3 positionWS, float3 normalWS)
+            {
+                float3 lightPositionWS = _OtherLightPositions[lightIndex].xyz;
+
+                float3 toLight = lightPositionWS - positionWS;
+
+                float3 L = normalize(toLight);
+
+                float NdotL = saturate( dot(normalWS, L) );
+
+                //衰减计算
+                float distanceSquared = max( dot(toLight, toLight), 0.00001);
+                float inverseRangeSquared = _OtherLightParams[lightIndex].x;
+
+                float rangeAttenuation = saturate(1.0 - distanceSquared * inverseRangeSquared);
+                rangeAttenuation *= rangeAttenuation;
+
+                return _OtherLightColors[lightIndex].rgb * NdotL * rangeAttenuation;
+            }
+
+
             Varyings Vert(Attributes input)
             {
                 Varyings output;
+
+                float4 posWS = mul(unity_ObjectToWorld, input.posOS);
+                output.posWS = posWS.xyz;
+
 
                 output.posCS = UnityObjectToClipPos(input.posOS);
 
@@ -79,14 +109,21 @@ Shader "MiniRP/MiniLit"
             {
                 float3 N = normalize(input.normalWS);
 
+                //平行光
                 float3 L = normalize(_MainLightDirection.xyz);
+                float mainNdotL  = saturate(dot(N, L));
+                float3 lighting = _BaseColor.rgb * _MainLightColor.rgb * mainNdotL;
 
-                //Lambert
-                float NdotL = saturate(dot(N, L));
+                //点光
+                for (int i = 0; i < _OtherLightCount;i++)
+                {
+                    lighting += CalculatePointLight( i, input.posWS, N);
+                }
 
-                float3 color = _BaseColor.rgb * _MainLightColor.rgb * NdotL;
 
-                return float4( color, _BaseColor.a);
+                float3 color = _BaseColor.rgb * lighting;
+
+                return float4( color, _BaseColor.a );
             }
 
             ENDHLSL
